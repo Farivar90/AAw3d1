445402dfc45cb35c71e1b3aaaff6f44570183bc7
farivar <farivaramiri@gmail.com>
2023-06-05 13:09:26 -0700

    third

--- enu.rb
+++ enu.rb
@@ -7,12 +7,37 @@ class Array
         end
         self
     end
-    def select(&prc)
+
+    def my_select(&prc)
         new_array = []
-        self.my_each(&prc) do |ele|
+        self.my_each do |ele|
             new_array << ele if prc.call(ele)
         end
         new_array
+    end
+
+    def my_reject(&prc)
+        new_array = []
+        self.my_each do |ele|
+            new_array << ele if !prc.call(ele)
+        end
+        new_array
+    end
+
+    def my_any?(&prc)
+        counter = 0
+        self.my_each do |ele|
+            counter += 1 if prc.call(ele)
+        end
+        counter > 0
+    end
+
+    def my_all?(&prc)
+        counter = 0
+        self.my_each do |ele|
+            counter += 1 if prc.call(ele)
+        end
+        counter == self.length
     end
     
 end
